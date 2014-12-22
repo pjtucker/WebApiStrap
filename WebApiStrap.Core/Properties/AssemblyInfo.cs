7a4b28b247e5b8650f27d32aaafeb4c7a0145194
Peter Tucker <peter.tucker@guaranteedrate.com>
2014-12-22 13:00:13 -0600

    Added spec file for nuget.

--- WebApiStrap.Core/Properties/AssemblyInfo.cs
+++ WebApiStrap.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebApiStrap.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Get up and running with a new ASP.Net WebAPI project quickly with this collection utilities for commonly used libraries.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WebApiStrap.Core")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
